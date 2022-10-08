9279418d169cc0832136248a27ac5349e8a4e21f
Lz <gthabard.py@gmail.com>
2022-10-09 00:32:16 +0200

    ancienne compétition + nouvelle compétition

---
+++ essential.py
@@ -0,0 +1,64 @@
+# -*- coding:Utf8 -*-
+#############################################
+# Programme Flask type
+# Auteur: G.T,Nt,2022
+#############################################
+# Importation de fonction externe :
+import json
+from datetime import datetime
+from flask import redirect, url_for
+#############################################
+# Définition des constantes
+MAX_BOOKED_PLACES = 12
+#############################################
+# Définition de fonction essentielle
+
+
+def load_competitions():
+    with open('competitions.json') as comps:
+        list_of_competitions = json.load(comps)['competitions']
+        return list_of_competitions
+
+
+def load_clubs():
+    with open('clubs.json') as c:
+        list_of_clubs = json.load(c)['clubs']
+        return list_of_clubs
+
+
+def sorted_old_competitions(competitions):
+    old_competitions = []
+    for competition in competitions:
+        competition_date = datetime.strptime(competition['date'], '%Y-%m-%d %H:%M:%S')
+        if competition_date < datetime.now():
+            old_competitions.append(competition)
+    return old_competitions
+
+
+def sorted_new_competitions(competitions):
+    new_competitions = []
+    for competition in competitions:
+        competition_date = datetime.strptime(competition['date'], '%Y-%m-%d %H:%M:%S')
+        if competition_date >= datetime.now():
+            new_competitions.append(competition)
+    return new_competitions
+
+
+def start_places_reserved(competitions, clubs):
+    places_reserved = []
+    for competition in competitions:
+        for club in clubs:
+            places_reserved.append({'competition': competition['name'], 'club': club['name'], 'placesRestantes': 0})
+    return places_reserved
+
+
+def update_places_reserved(competition, club, places_reserved, places_required):
+    for item in places_reserved:
+        if item['competition'] == competition['name'] and item['club'] == club['name']:
+            if item['placesRestantes'] + places_required <= MAX_BOOKED_PLACES:
+                item['placesRestantes'] += places_required
+            else:
+                raise ValueError("vous ne pouvez pas réserver plus de 12 places")
+    return places_reserved
+
+
--- server.py
+++ server.py
@@ -1,12 +1,40 @@
-from flask import Flask, render_template, request, redirect, flash, url_for
-from load_json import load_clubs, load_competitions
+# -*- coding:Utf8 -*-
+#############################################
+# Programme Flask type
+# Auteur: G.T,Nt,2022
+#############################################
+# Importation de fonction externe :
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    flash,
+    url_for)
+from essential import (
+    load_clubs,
+    load_competitions,
+    sorted_old_competitions,
+    sorted_new_competitions,
+    start_places_reserved,
+    update_places_reserved)
+from datetime import datetime
+#############################################
+# Définition des constantes
+MAX_BOOKED_PLACES = 12
+#############################################
+# Création de l'application
 
 
 def create_app():
     app = Flask(__name__)
     app.secret_key = 'something_special'
+
     competitions = load_competitions()
     clubs = load_clubs()
+    new_competitions = sorted_new_competitions(competitions)
+    old_competitions = sorted_old_competitions(competitions)
+    places_reserved = start_places_reserved(competitions, clubs)
 
     @app.route('/')
     def index():
@@ -14,41 +42,93 @@ def create_app():
 
     @app.route('/show_summary', methods=['POST'])
     def show_summary():
-        try:
-            club = [club for club in clubs if club['email'] == request.form['email']][0]
-        except (IndexError, KeyError):
-            return page_not_found()
-        return render_template('welcome.html', club=club, competitions=competitions)
+        if request.method == 'POST':
+            try:
+                club = [club for club in clubs if club['email'] == request.form['email']][0]
+                return render_template('welcome.html',
+                                       club=club,
+                                       new_competitions=new_competitions,
+                                       old_competitions=old_competitions)
+            except IndexError:
+                if request.form['email'] == '':
+                    flash('Veuillez rentrer une adresse mail.', 'error')
+                else:
+                    flash("Impossible de vous authentifier adresse mail inconnu.", 'error')
+                return render_template('index.html')
 
     @app.route('/book/<competition>/<club>')
     def book(competition, club):
-        found_club = [c for c in clubs if c['name'] == club][0]
-        found_competition = [c for c in competitions if c['name'] == competition][0]
-        if found_club and found_competition:
-            return render_template('booking.html', club=found_club, competition=found_competition)
-        else:
-            flash("Something went wrong-please try again")
-            return render_template('welcome.html', club=club, competitions=competitions)
+        found_club = [regional_club for regional_club in clubs
+                      if regional_club['name'] == club][0]
+        try:
+            found_competition = [regional_competition for regional_competition in competitions
+                                 if regional_competition['name'] == competition][0]
+
+            if datetime.strptime(found_competition['date'], '%Y-%m-%d %H:%M:%S') < datetime.now():
+                flash("la competitions est terminé", 'error')
+
+            else:
+                return render_template('booking.html',
+                                       club=found_club,
+                                       competition=found_competition)
+        except IndexError:
+            flash("Il y a un problème", 'error')
+
+        return render_template('welcome.html', club=found_club,
+                               new_competitions=new_competitions,
+                               old_competitions=old_competitions)
 
     @app.route('/purchase_places', methods=['POST'])
     def purchase_places():
-        competition = [c for c in competitions if c['name'] == request.form['competition']][0]
-        club = [c for c in clubs if c['name'] == request.form['club']][0]
-        places_required = int(request.form['places'])
-        competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - places_required
-        flash('Great-booking complete!')
-        return render_template('welcome.html', club=club, competitions=competitions)
+        competition = [regional_competition for regional_competition in competitions
+                       if regional_competition['name'] == request.form['competition']][0]
+        club = [regional_club for regional_club in clubs
+                if regional_club['name'] == request.form['club']][0]
+        if request.method == 'POST':
+            try:
+                places_required = int(request.form['places'])
+                if places_required > int(competition['numberOfPlaces']):
+                    flash(f"Cette compétition dispose de {int(competition['numberOfPlaces'])}"
+                          f" places vous ne pouvez donc pas réservé {places_required} places.", 'error')
+                elif places_required > int(club['points']):
+                    flash(f"Votre club dispose de {int(club['points'])}"
+                          f" points vous ne pouvez donc pas réservé {places_required} places.", 'error')
+                elif places_required > MAX_BOOKED_PLACES:
+                    flash(f"Vous ne pouvez pas réservé plus de {MAX_BOOKED_PLACES} places par compétitions.", 'error')
+                else:
+                    try:
+                        update_places_reserved(competition, club, places_reserved, places_required)
+                        competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - places_required
+                        club['points'] = int(club['points']) - places_required
+                        flash(f'Vous avez réservé {places_required} places.', 'success')
+
+                        return render_template('welcome.html',
+                                               club=club,
+                                               new_competitions=new_competitions,
+                                               old_competitions=old_competitions)
+
+                    except ValueError as first_error:
+                        flash(f"{first_error}", 'error')
+
+            except ValueError:
+                flash(f'Veuillez saisir une valeur numérique comprise entre 1 et 12', 'error')
+
+            return render_template('booking.html',
+                                   club=club,
+                                   competition=competition)
 
     @app.route('/logout')
     def logout():
         return redirect(url_for('index'))
 
-    @app.errorhandler(404)
-    def page_not_found():
-        return render_template('404.html'), 404
+    @app.route('/tableau_points')
+    def tableau_points():
+        sorted_clubs = sorted(clubs, key=lambda club: int(club['points']), reverse=True)
+        return render_template('tableau_points.html', clubs=sorted_clubs)
 
     return app
 
 
-if __name__ == '__server__':
-    create_app().run()
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
