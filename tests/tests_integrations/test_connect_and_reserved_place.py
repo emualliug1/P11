6c7b88ea0de1c834d5b1b0f4955d49bf9b228e60
Lz <gthabard.py@gmail.com>
2022-10-22 13:59:30 +0200

    fin du projet

--- tests/tests_integrations/test_connect_and_reserved_place.py
+++ tests/tests_integrations/test_connect_and_reserved_place.py
@@ -7,11 +7,16 @@ class TestLoginAndReservedPlaces:
         self.clubs = load_clubs()
 
     def test_login_and_reserve(self, client):
-        credential = {'email': self.clubs[0]['email']}
-        response = client.post('/show_summary', data=credential)
-
+        credential = {'email': self.clubs[3]['email']}
+        client.post('/show_summary', data=credential)
+        response = client.post('/purchase_places',
+                    data={"competition": self.competitions[3]['name'],
+                          "club": self.clubs[3]['name'],
+                          "places": 2})
+        expected_points = '498'
         assert response.status_code == 200
-
+        data = response.data.decode()
+        assert expected_points in data
 
 
 
